353d51572dd7a9b8d3a1afb65268a4792234bf28
Preetii17 <pree18293@gmail.com>
2026-07-03 17:16:05 +0530

    Initial commit

---
+++ basics/variables.js
@@ -0,0 +1,33 @@
+// VARIABLES IN JAVASCRIPT
+
+// DEFINITION
+// Variables are containers used to store data in javascript.
+
+// There are three ways to declare variables:
+// 1. var   - Function scoped (older way)
+// 2. let   - Block scoped (recommended for values that change)
+// 3. const - Block scoped (used for values that should not change)
+
+//SYNTAX
+// var variableName = value;
+// let variableName = value;
+// const variableName = value;
+
+//Examples
+
+//1. var
+
+var language="javascript";
+console.log(language);  // output: javascript
+
+//2. let
+
+let name="Emma";
+console.log(name);      // output: Emma
+
+//3. const
+
+const age=22;
+console.log(age);       // output: 22
+
+
